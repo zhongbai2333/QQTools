7fce0908beb162c13ac267ce7a037fde448a5804
zhong_bai <3422857988@qq.com>
2023-06-18 18:14:09 +0800

    update one test

--- qq_tools/__init__.py
+++ qq_tools/__init__.py
@@ -1,21 +1,21 @@
 import requests
 from flask import Flask, request
-
-'''注意，这里的import api是另一个py文件，下文会提及'''
+from mcdreforged.api.all import *
 
 app = Flask(__name__)
-
 '''监听端口，获取QQ信息'''
 
 
 @app.route('/', methods=["POST"])
-def post_data():
+def post_data(server: PluginServerInterface):
     if request.get_json().get('meta_event_type') != 'heartbeat':
-        alljson = request.get_json()
+        alljson = str(request.get_json())
+        server.say(alljson)
         gid = request.get_json().get('group_id')
         msg = request.get_json().get('raw_message')
-        print(alljson)
-        requests.get(url='http://127.0.0.1:5700/send_group_msg?group_id={0}&message={1}'.format(gid, msg))
+        if gid == 816676892:
+            server.say(msg)
+        #requests.get(url='http://127.0.0.1:5700/send_group_msg?group_id={0}&message={1}'.format(gid, msg))
     '''下面的request.get_json().get......是用来获取关键字的值用的，关键字参考上面代码段的数据格式
     if request.get_json().get('message_type') == 'private':  # 如果是私聊信息
         uid = request.get_json().get('sender').get('user_id')  # 获取信息发送者的 QQ号码
@@ -29,5 +29,10 @@ def post_data():
     return 'OK'
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5701)  # 此处的 host和 port对应上面 yml文件的设置
+@new_thread('QQListen')
+def start_listen():
+    app.run(debug=True, host='127.0.0.1', port=5701)
+
+
+def on_load(server, prev):
+    start_listen()
