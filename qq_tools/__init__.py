24c703fc1d547da6edd991b8c814281ac43264be
zhong_bai <3422857988@qq.com>
2023-06-19 22:23:03 +0800

    update

--- qq_tools/__init__.py
+++ qq_tools/__init__.py
@@ -1,29 +1,54 @@
 import requests
 import json
-from mcdreforged.api.all import *
-from mcdreforged.api.types import PluginServerInterface
-from wsgiref.simple_server import make_server
 import re
-
+import time
+from mcdreforged.api.all import *
+from typing import List, Dict
+from wsgiref.simple_server import make_server
 
 global httpd
+__mcdr_server: PluginServerInterface
 
 
-def on_load(server, prev):
-    cq_listen(True)
+class Config(Serializable):
+    send_host: str = "127.0.0.1"
+    send_port: int = 5700
+    post_host: str = "0.0.0.0"
+    post_port: int = 5701
+    groups: List[int] = [00000000, 11111111]
+    admins: List[int] = [00000000, 11111111]
+    whitelist_add_with_bound: bool = True
+    whitelist_remove_with_leave: bool = True
+    forwards: Dict[str, bool] = {
+        'mc_to_qq': False,
+        'qq_to_mc': False
+    }
+    commands: Dict[str, bool] = {
+        'list': True
+    }
+
+
+def on_load(server: PluginServerInterface, prev_module):
+    global __mcdr_server
+    __mcdr_server = server  # mcdr init
+    config = server.load_config_simple(target_class=Config)  # Get Config setting
+    cq_listen(config.post_host, config.post_port)
+
+
 # requests.get(url='http://127.0.0.1:5700/send_group_msg?group_id={0}&message={1}'.format(gid, msg))
 
 
-def on_unload(server):
+def on_unload(server: PluginServerInterface):
     httpd.shutdown()
+    __mcdr_server.logger.info("Http server stopping now...")
+    time.sleep(0.5)
 
 
 @new_thread('QQListen')
-def cq_listen(start):
+def cq_listen(host: str, port: int):
     global httpd
-    port = 5701
-    httpd = make_server("0.0.0.0", port, application)
-    print("serving http on port {0}...".format(str(port)))
+    httpd = make_server(host, port, application)
+    __mcdr_server.logger.info("Serving http on port {0}...".format(str(port)))
     httpd.serve_forever()
 
 
